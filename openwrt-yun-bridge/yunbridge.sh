17b42e9ddc39cfa43e9202f524c558f2a2c56913
Ignacio Santolin <ignacio.santolin@gmail.com>
2026-01-03 06:56:56 -0300

    Align procd init with yunbridge wrapper
    - Start procd service via /usr/bin/yunbridge to ensure PYTHONPATH is set
    - Run wrapper with -O and yunbridge.daemon entrypoint

--- openwrt-yun-bridge/yunbridge.sh
+++ openwrt-yun-bridge/yunbridge.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Wrapper that seeds PYTHONPATH before starting the YunBridge daemon.
 export PYTHONPATH=/usr/libexec/yunbridge${PYTHONPATH:+:$PYTHONPATH}
-exec /usr/bin/python3 -m yunbridge.daemon "$@"
+exec /usr/bin/python3 -O -m yunbridge.daemon "$@"

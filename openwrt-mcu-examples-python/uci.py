db97027951f78f81a48f88bc051c7a36e8d0bf8f
Ignacio Santolin <ignacio.santolin@gmail.com>
2026-01-13 22:46:55 -0300

    docs: add mock UCI for local testing and secure credentials
    This adds a mock uci.py to allow running examples locally without OpenWrt. It uses environment variables for credentials to avoid hardcoding secrets. Also includes version bumps.

---
+++ openwrt-mcu-examples-python/uci.py
@@ -0,0 +1,20 @@
+import os
+
+class Uci:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+    def get_all(self, package, section):
+        if package == "mcubridge" and section == "general":
+            return {
+                "mqtt_host": os.environ.get("MQTT_HOST", "192.168.15.36"),
+                "mqtt_port": os.environ.get("MQTT_PORT", "8883"),
+                "mqtt_tls": "1",
+                "mqtt_tls_insecure": "1",
+                "mqtt_user": os.environ.get("MQTT_USER", "ignacio.santolin"),
+                "mqtt_pass": os.environ.get("MQTT_PASS", "placeholder_password"),
+            }
+        return {}

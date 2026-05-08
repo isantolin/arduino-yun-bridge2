e82081e9b077199f5a9762fa527a4d843b0d7b57
Ignacio Santolin <ignacio.santolin@gmail.com>
2026-05-08 20:31:56 -0300

    chore: remove temporary test files

--- test_etl.cpp
+++
@@ -1,4 +0,0 @@
-#include "mcubridge-library-arduino/src/etl_profile.h"
-#include <initializer_list>
-#include <etl/algorithm.h>
-int main() { return 0; }
--- test_etl2.cpp
+++
@@ -1,4 +0,0 @@
-#define ETL_NO_STL
-#define ETL_NO_INITIALIZER_LIST
-#include <etl/algorithm.h>
-int main() { return 0; }
